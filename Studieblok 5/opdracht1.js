ba59f781e5e0a65f5f95fed50664348a3e61119b
Sander Cokart <112039@student.drenthecollege.nl>
2019-01-29 16:05:25 +0100

    improved regex

--- Studieblok 5/opdracht1.js
+++ Studieblok 5/opdracht1.js
@@ -1,9 +1,9 @@
 function Validate(){
     var regexPost = "/[0-9]{4}[a-z]{2}/i";
-    var regexEmail = "/.*@.*\..*/i";
+    var regexEmail = "/^.*@.*\\..*$/";
     var regexPhone = "/[0-9]{10}/";
-    var regexAdres = "/[a-z]* [0-9]*/i";
-    var regexGender = "/(fe)?male/i";
+    var regexAdres = "/^[a-z]* [0-9]*$/i";
+    var regexGender = "/^(fe)?male$/i";
 
     document.write (regexPost+"<br>");
     document.write (regexEmail+"<br>");
